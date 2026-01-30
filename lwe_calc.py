72587f1ce283ee12e931c382979754b4ea17c062
Okumura Hiroaki <Okumura.h@northeastern.edu>
2026-01-29 23:06:39 -0500

    commit

--- lwe_calc.py
+++
@@ -1,9 +0,0 @@
-#import numpy
-
-"""
-Numpy calculation for LWE, basis for understanding PQC, and the mathematics behind it. 
-"""
-
-#implement LWE and make it into an algorithm to produce, encrypted keys in a 2-3 dimensional plane
-
-#num: int = np.random.randint(10, 20), careful and never make this into your random generator, because this is not truly random
