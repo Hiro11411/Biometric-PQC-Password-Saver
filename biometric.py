8f538ea0bf48ce835983e651a1c537d0e4fc2e36
Okumura Hiroaki <Okumura.h@northeastern.edu>
2026-02-09 14:22:06 -0500

    work flow change

--- biometric.py
+++ biometric.py
@@ -180,66 +180,71 @@ class BiometricSystem:
             print(f"Distance is ${distance}") #might want to consider threshold here
             max_same_person = max(distances)
             print(f"Set threshold above: {max_same_person}") #for debugging and setting purposes right now
-            return distance
-        # print(json.dumps(result, indent=2)) #result is a dict
+        
+        #================================================================================================================================
+        #Threshold Comparison
+        #========================================================================================================================================
+
+            if 0.6 <= distance <= 0.8: #Safest distance range for threshold
+                return distance
+            else:
+                print(f"This euclidian distance doesn't fit the threshold ${distance}")
+                return 0
 
         os.remove(temp_path)
 
-#================================================================================================================================
-#Threshold Comparison
-#================================================================================================================================
+# #Only used for testing within local
+#     def main(self):
+#         #call both functions in main
 
-    def main(self):
-        #call both functions in main
+#         id = self.id_input() #gets the original ID, and inputs here
+#         count = 0 #counter
+#         while True: #Main Loop
+#             try:     
+#                 ret, frame = self.video.read() #when camera empty, _src.empty() in func
 
-        id = self.id_input() #gets the original ID, and inputs here
-        count = 0 #counter
-        while True: #Main Loop
-            try:     
-                ret, frame = self.video.read() #when camera empty, _src.empty() in func
+#                 if not ret: #meaning that if Camera doesn't work continues running 
+#                     video.read() #fix here later main problem
+#                     continue
 
-                if not ret: #meaning that if Camera doesn't work continues running 
-                    video.read() #fix here later main problem
-                    continue
+#                 gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #START IN GRAY COLOR, using BGR FORMAT NOT RGB
 
-                gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #START IN GRAY COLOR, using BGR FORMAT NOT RGB
-
-                faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
-                for (x, y, w, h) in faces: #tuple, takes tuple data and uses it within rectangle
-                    count=count+1 #counter to count the amount of photos taken, then translate
+#                 faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
+#                 for (x, y, w, h) in faces: #tuple, takes tuple data and uses it within rectangle
+#                     count=count+1 #counter to count the amount of photos taken, then translate
                         
-                    face_roi = frame[y:y+h, x:x+w]#reigon of interest, face
+#                     face_roi = frame[y:y+h, x:x+w]#reigon of interest, face
 
-                    emotion_dominant, confidence = self.emotional_analyzer(face_roi) #takes in ONLY face_roi, returns variables and determines theresholds
-                    #passing parameter succesfully here.
+#                     emotion_dominant, confidence = self.emotional_analyzer(face_roi) #takes in ONLY face_roi, returns variables and determines theresholds
+#                     #passing parameter succesfully here.
 
-                    cv2.imwrite('datasets/User.'+str(id)+"."+str(count)+".jpg", gray[y:y+h, x:x+w])
-                    cv2.rectangle(frame, (x,y), (x+w, y+h), (50, 50, 225), 1) #tuple data used within here
-                    cv2.putText(frame, emotion_dominant, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2) #emotion shown
+#                     cv2.imwrite('datasets/User.'+str(id)+"."+str(count)+".jpg", gray[y:y+h, x:x+w])
+#                     cv2.rectangle(frame, (x,y), (x+w, y+h), (50, 50, 225), 1) #tuple data used within here
+#                     cv2.putText(frame, emotion_dominant, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2) #emotion shown
 
-                    #constructs rectange
+#                     #constructs rectange
 
-                    cv2.imshow("Frame", frame)
+#                     cv2.imshow("Frame", frame)
                     
-                    key = cv2.waitKey(1) #waiting for key press in 1 ms, if key press then ..
+#                     key = cv2.waitKey(1) #waiting for key press in 1 ms, if key press then ..
                     
-                    if key==ord('q'):
-                        break
-                    # if count > 500: #deletes program, when q key is pressed
-                    #     break #break program
+#                     if key==ord('q'):
+#                         break
+#                     # if count > 500: #deletes program, when q key is pressed
+#                     #     break #break program
 
-                    #remember to handle errors !!!!!!!! Important asf
+#                     #remember to handle errors !!!!!!!! Important asf
 
-                    encryption() #mainly work on functions here tmr!!
-                    decryption()
-            except cv2.error as e:
-                print(f"CV2 ERR {e}")
-                continue
+#                     encryption() #mainly work on functions here tmr!!
+#                     decryption()
+#             except cv2.error as e:
+#                 print(f"CV2 ERR {e}")
+#                 continue
 
-video.release() #after done closes the webcam connectio
+# video.release() #after done closes the webcam connectio
 
-cv2.destroyAllWindows() #Closes all OpenCV windows
-print("Closed File, Frame")
+# cv2.destroyAllWindows() #Closes all OpenCV windows
+# print("Closed File, Frame")
 #should implement this here with main function, better for script, and safer/more efficient
 
 #encoding for first time vs encoding for second time
@@ -247,4 +252,4 @@ print("Closed File, Frame")
 #runs script
 if __name__ == '__main__':
     system = BiometricSystem()
-    system.main()
+    # system.main()
